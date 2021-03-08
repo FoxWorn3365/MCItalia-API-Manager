14bf3b701b2b7fa19e57ad7d65a4175d9d123eb9
FoxWorn3365 <61429263+FoxWorn3365@users.noreply.github.com>
2021-03-08 20:18:35 +0100

    Aggiornato il file index_example.php

--- index_example.php
+++ index_example.php
@@ -2,11 +2,9 @@
 // Includo il loader.php
 include("loader.php");
 
-// Eseguo la funzione
-// Ricordo che per far andare correttamente il programma bisogna inserire lo slug
-recupera("slimefun");
+// Per decidere quale server controllare si è pregati di inserire
+// lo slug del server di Minecraft Italia nel file conf.key
 
 // Ora vediamo il nostro output
 echo "Nel server $nome ci sono $online/$max player online. I voti di ieri sono $voti_ieri mentre i voti totali di oggi sono $voti. Il server è nella versione $versione";
-
 ?>
