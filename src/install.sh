89f0a1083d4bfa24e5476ab050929883f8e3b71f
FoxWorn3365 <61429263+FoxWorn3365@users.noreply.github.com>
2021-03-08 19:53:23 +0100

    Creato l'installer universale per Linux

---
+++ src/install.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+echo "
+///////////////////////////
+ Pacchetto di Installazione
+/////// LINUX-UN_01 ///////
+///////////////////////////
+
+Stai per installare:
+MCItalia-API-Manager (loader.php)
+
+> Iniziare l'installazione? [Y/Ctrl+C]
+";
+read()
+
+wget (https://raw.githubusercontent.com/FoxWorn3365/MCItalia-API-Manager/main/src/loader.php)
+
+echo "FILE SCARICATO CON SUCCESSO!"
+
+
+
