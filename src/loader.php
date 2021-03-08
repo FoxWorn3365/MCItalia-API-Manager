7fb5d8641f7b2f3d0a7943d9d4d31c8d05dcd4a4
FoxWorn3365 <61429263+FoxWorn3365@users.noreply.github.com>
2021-03-08 20:19:21 +0100

    Aggiornato il loader.php per prevenire bug e crash

--- src/loader.php
+++ src/loader.php
@@ -5,37 +5,24 @@
 // By FoxWorn3365 | © 2021 Cosma F //
 /////////////////////////////////////
 
-function recupera($nomeserver) {
-  // Starto CURL
-  $ch = curl_init();
+$nomeserver = file_get_contents('config.key');
 
-  // Imposto la pagina che dovrà essere analizzata per recuperare il .json
-  curl_setopt($ch, CURLOPT_URL, 'http://www.sito.com/pagina.html');
+$testo = file_get_contents('https://api.minecraft-italia.it/v5/server-info/' .$nomeserver);
 
-  // Imposto che gli header non vengano scarcati
-  curl_setopt($ch, CURLOPT_HEADER, 0);
-
-  // Eseguo CURL, salvando il risultato in una variabile
-  $mcita = curl_exec($ch);
-
-  // Chiudo CURL
-  curl_close($ch);
+$output = json_decode($testo);
   
-  // Uso json_decode
-  $output = json_decode($mcita);
-  
-  // Recupero i dati
-  $nome = $output->name;
-  $indirizzo = $output->address;
-  $premium = $output->premium;
-  $posizione = $output->position;
-  $voti = $output-> votesTotal;
-  $voti_ieri = $output->votesYesterday;
-  $versione = $output->versionsString;
-  $online = $output->online;
-  $players = $output->players;
-  $max = $output->slot;
-}
+// Recupero i dati
+$nome = $output->name;
+$indirizzo = $output->address;
+$premium = $output->premium;
+$posizione = $output->position;
+$voti = $output-> votesTotal;
+$voti_ieri = $output->votesYesterday;
+$versione = $output->versionsString;
+$online = $output->online;
+$players = $output->players;
+$max = $output->slot;
+
 
 // RILASCIATO SOTTO LICENZA MIT
 
